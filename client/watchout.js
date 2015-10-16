d5342d4db50e83c9062cb6aa659b5d52c0cfbc4c
ctodmia <ctodmia@gmail.com>
2015-10-16 16:24:33 -0700

    asteroids show up on screen!

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,74 @@
-// start slingin' some d3 here.
+// var gameOptions = {
+//     height: 450,
+//     width: 700,
+//     nEnemies: 30,
+//     padding: 20
+// }
+
+// var svg = d3.select("gameboard").append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
+
+var enemies = _.range(0, 30)/* array of 30 enemies with random x and y coordinates*/;
+
+var width = 960,
+    height = 500,
+    nEnemies = 30 
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    // .attr("transform", "translate(32," + (height / 2) + ")");
+
+var imgs = svg.selectAll("img").data([0]);
+            imgs.enter()
+            .append("svg:img")
+            .attr("xlink:href", "asteroid.png")
+
+function update(data) {
+
+  // DATA JOIN
+  // Join new data with old elements, if any.
+  var text = svg.selectAll("text")
+      .data(data, function(d) { return d; });
+
+  // UPDATE
+  // Update old elements as needed.
+  text.attr("class", "update");
+
+  // ENTER
+  // Create new elements as needed.
+  //svg.selectAll("text")
+  //    .data(data, function(d) { return d; });
+  text.enter().append("svg:image")
+      .attr("class", "enter")
+      // .append("svg:image")
+      .attr("xlink:href", "asteroid.png")
+      //.attr("dy", ".35em")
+      .attr("height", "100px")
+      .attr("width", "100px")
+      .text(function(d) { return d; });
+  // text.enter().selectAll("image").data([0])
+
+
+  // ENTER + UPDATE
+  // Appending to the enter selection expands the update selection to include
+  // entering elements; so, operations on the update selection after appending to
+  // the enter selection will apply to both entering and updating nodes.
+  text.attr("x", function(d, i) { return Math.floor(Math.random()*960); })
+    .attr("y", function(d, i) { return Math.floor(Math.random()*500);})
+
+  // EXIT
+  // Remove old elements as needed.
+  text.exit().remove();
+}
+
+// The initial display.
+update(enemies);
+
+// Grab a random sample of letters from the alphabet, in alphabetical order.
+setInterval(function() {
+  update(d3.shuffle(enemies)
+      .slice(0, Math.floor(Math.random() * 26))
+      .sort());
+}, 1500);
 
